6ccba993671a4e4c9bdccd09a690d1b60bc053a5
Patrick Altman <paltman@gmail.com>
2013-07-13 19:49:03 -0500

    Start handler tests

--- tests/tests.js
+++ tests/tests.js
@@ -236,3 +236,33 @@ test("form.submit with 503 status code fires eldarion-ajax:complete", 3, functio
     this.server.respond();
     $(document).off("eldarion-ajax:complete");
 });
+
+
+module("Handler Tests (jQuery " + jQuery.fn.jquery + ")", {
+    setup: function () {
+        var testData = {"html": "My good content"};
+        this.server = sinon.fakeServer.create();
+        this.server.respondWith("GET", "/test/message/", [200, {"Content-Type": "application/json"}, JSON.stringify(testData)]);
+        this.server.respondWith("POST", "/test/message/no-data/", [200, {"Content-Type": "text/html"}, ""]);
+        this.server.respondWith("GET", "/test/message/400/", [400, {"Content-Type": "application/json"}, JSON.stringify(testData)]);
+        this.server.respondWith("GET", "/test/message/404/", [404, {"Content-Type": "application/json"}, JSON.stringify(testData)]);
+        this.server.respondWith("GET", "/test/message/500/", [500, {"Content-Type": "application/json"}, JSON.stringify(testData)]);
+        this.server.respondWith("GET", "/test/message/503/", [503, {"Content-Type": "application/json"}, JSON.stringify(testData)]);
+        this.server.respondWith("POST", "/create/message/", [200, {"Content-Type": "application/json"}, JSON.stringify(testData)]);
+        this.server.respondWith("POST", "/create/message/400/", [400, {"Content-Type": "application/json"}, JSON.stringify(testData)]);
+        this.server.respondWith("POST", "/create/message/404/", [404, {"Content-Type": "application/json"}, JSON.stringify(testData)]);
+        this.server.respondWith("POST", "/create/message/500/", [500, {"Content-Type": "application/json"}, JSON.stringify(testData)]);
+        this.server.respondWith("POST", "/create/message/503/", [503, {"Content-Type": "application/json"}, JSON.stringify(testData)]);
+    },
+    teardown: function () {
+        this.server.restore();
+        $(".message").html("");
+    }
+});
+
+test("a.click with data-replace-inner response populates div.message", 1, function () {
+    $("#qunit-fixture a.message-get").trigger("click");
+    this.server.respond();
+    equal($(".message").html(), "My good content", "div.message filled with correct data");
+});
+
